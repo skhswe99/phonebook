f92e616e00117a462f25586e259b372113a5bab7
skhswe99 <skhswe99@gmail.com>
2023-11-22 21:52:33 -0800

    enable adding names to phonebook

--- src/App.js
+++ src/App.js
@@ -4,12 +4,24 @@ const App = () => {
   const [persons, setPersons] = useState([{ name: 'Arto Hellas' }])
   const [newName, setNewName] = useState('')
 
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    if (newName) { 
+      setPersons([...persons, {name: newName}]) // add new name to phonebook
+    }
+    setNewName('') // clear input field
+  }
+
+  const handleNewName = (event) => {
+    setNewName(event.target.value)
+  }
+
   return (
     <div>
      <h2>Phonebook</h2>
-     <form>
+     <form onSubmit={handleSubmit}>
       <div>
-        name: <input />
+        name: <input value={newName} onChange={handleNewName} /> {/* controlled input */}
       </div>
       <div>
       <button type="submit">add</button>
@@ -17,7 +29,7 @@ const App = () => {
     </form>
     
     <h2>Numbers</h2>
-      ...
+      {persons.map(person => <li key={person.name}>{person.name}</li>)}
     </div>
   )
 }
