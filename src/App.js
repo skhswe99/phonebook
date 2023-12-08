fac723a01dad344be0ada2de113ae70ed4d1a334
skhswe99 <skhswe99@gmail.com>
2023-12-08 10:31:11 -0800

    fecth phonebook entrires from db.json with axios and effect hook

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Filter from './components/Filter/Filter'
 import PersonForm from './components/PersonForm/PersonForm'
 import Persons from './components/Persons/Persons'
-
+import axios from 'axios'
 
 const App = () => {
 
-  const [persons, setPersons] = useState([{ name: 'Arto Hellas', phone: '040-123456' },
-                                          { name: 'Ada Lovelace', number: '39-44.5323523', id: 2},
-                                          { name: 'Dan Abramov', number: '12-43-234345', id: 3},
-                                          { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4}
-                                          ])
+  // const [persons, setPersons] = useState([{ name: 'Arto Hellas', phone: '040-123456' },
+  //                                         { name: 'Ada Lovelace', number: '39-44.5323523', id: 2},
+  //                                         { name: 'Dan Abramov', number: '12-43-234345', id: 3},
+  //                                         { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4}
+  //                                         ])
+  
+  const [persons, setPersons] = useState([])
+  useEffect( () => {
+    axios.get('http://localhost:3001/persons')
+          .then(res => setPersons(res.data))
+  }, [])
+  
   const [newName, setNewName] = useState('')
   const [newPhone, setNewPhone] = useState('')
   const [filter, setFilter] = useState('')
